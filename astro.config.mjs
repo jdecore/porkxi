315cd6ee96d724edeea9c9afd3e850035e56e124
jdenriquezr <jdenriquezr@gmail.com>
2026-04-02 16:18:40 -0500

    inicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
 
 export default defineConfig({
-  integrations: [
-    vue({
-      appEntrypoint: '/src/vue-app.ts',
-    }),
-  ],
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: 'always',
   },
 });
--- src/vue-app.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-
-export default (app) => {
-  return app;
-};
