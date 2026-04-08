927fc2712d3b6e96f0634faad8aebe2875a80920
jdenriquezr <jdenriquezr@gmail.com>
2026-04-07 22:15:01 -0500

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,8 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
 export default defineConfig({
-  integrations: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.includes('-'),
-        }
-      }
-    })
-  ],
+  integrations: [ vue() ],
   build: {
     inlineStylesheets: 'always',
   },
-  experimental: {
-    clientPrerender: false
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        external: ['astro:components']
-      }
-    }
-  }
 });
